8383cf992749e9d824f1410a427e3dca0900bc34
Ashish0207 <Ashish_0207@yahoo.co.in>
2022-08-07 22:23:06 +0530

    Add files via upload

---
+++ src/main/java/com/devops/ashish/sample/App.java
@@ -0,0 +1,13 @@
+package com.devops.ashish.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "DevOps Playground" );
+    }
+}
